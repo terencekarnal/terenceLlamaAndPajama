fd2b92f908ed72a1eed9f6a1cddaaeebf503e795
Terence Fox-Karnal <tkarnal@gmail.com>
2015-04-30 15:11:07 -0400

    first llama and pajama commit

--- Llamas in Pajamas/GameScene.swift
+++ Llamas in Pajamas/GameScene.swift
@@ -86,6 +86,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //TODO: Add characters to the scene
         
+    for _ in 0..<4 {
+            let pajama = Pajama()
+            pajama.pajamaColor = PajamaColor.red
+            addPajama(pajama)
+        }
+    
+    for _ in 0..<4 {
+            let pajama = Pajama()
+            pajama.pajamaColor = PajamaColor.yellow
+            addPajama(pajama)
+        }
+        
+    for _ in 0..<2 {
+            let pajama = Pajama()
+            pajama.pajamaColor = PajamaColor.blue
+            addPajama(pajama)
+        }
+        
+    let lion = Lion(runningSpeedInSeconds: 1.2)
+        lion.position = CGPointMake(60, 100)
+        addChild(lion)
+    
+    let lion2 = Lion(runningSpeedInSeconds: 0.9)
+        lion2.position = CGPointMake(60, 200)
+        addChild(lion2)
+    
+    let lion3 = Lion(runningSpeedInSeconds: 0.6)
+        lion3.position = CGPointMake(60, 275)
+        addChild(lion3)
+        
     }
     
     func addPajama(pajama: Pajama) {
